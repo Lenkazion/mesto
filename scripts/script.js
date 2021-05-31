7300cd1fbb961a74ee58394db60dd3e159a5dfba
Lenka <hollycia@yandex.ru>
2021-05-31 23:19:29 +0200

    Ready for first review

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+let popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let inputName = document.querySelector('.popup__input_name');
+let inputDescription = document.querySelector('.popup__input_description');
+let openPopup = document.querySelector('.profile__edit-button');
+let submitButton = document.querySelector('.popup__submit');
+let closeButton = document.querySelector('.popup__close'); 
+
+
+
+openPopup.addEventListener('click', function(evt){
+    evt.preventDefault();
+    popup.classList.add('popup_opened');
+    inputName.value = profileName.textContent;
+    inputDescription.value = profileDescription.textContent;
+});
+
+submitButton.addEventListener('click', function(evt){
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+    popup.classList.remove('popup_opened');
+})
+
+closeButton.addEventListener('click', function(evt){
+    evt.preventDefault();
+    popup.classList.remove('popup_opened');
+});
