30bc51f1ad86d49525fbabd1bfe25d28155ef8ce
Lenka <hollycia@yandex.ru>
2021-06-03 00:44:01 +0200

    2nd Review Version

--- scripts/script.js
+++ scripts/script.js
@@ -3,27 +3,34 @@ let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let inputName = document.querySelector('.popup__input_type_name');
 let inputDescription = document.querySelector('.popup__input_type_description');
-let openPopup = document.querySelector('.profile__edit-button');
+let editProfileButton = document.querySelector('.profile__edit-button');
 let submitButton = document.querySelector('.popup__submit');
-let closeButton = document.querySelector('.popup__close'); 
+let closeButton = document.querySelector('.popup__close');
+let form = document.querySelector('.popup__form'); 
 
-
-
-openPopup.addEventListener('click', function(evt){
-    evt.preventDefault();
-    popup.classList.add('popup_opened');
+function writePopup () {
     inputName.value = profileName.textContent;
     inputDescription.value = profileDescription.textContent;
-});
+};
 
-submitButton.addEventListener('click', function(evt){
+function openPopup () {
+    popup.classList.add('popup_opened');
+    writePopup();
+}
+
+editProfileButton.addEventListener('click', openPopup);
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+closeButton.addEventListener('click', closePopup);
+
+function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileDescription.textContent = inputDescription.value;
-    popup.classList.remove('popup_opened');
-})
+    closePopup();
+}
 
-closeButton.addEventListener('click', function(evt){
-    evt.preventDefault();
-    popup.classList.remove('popup_opened');
-});
+form.addEventListener('submit', formSubmitHandler);
